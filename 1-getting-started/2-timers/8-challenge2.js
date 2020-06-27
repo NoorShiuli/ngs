6dec9d96878efe98995b56c4aa67f27a333066fc
shiuli <ext_salam@Falabella.cl>
2020-06-27 18:05:30 +0530

    WIP

--- 1-getting-started/2-timers/8-challenge2.js
+++ 1-getting-started/2-timers/8-challenge2.js
@@ -3,3 +3,18 @@
 // And stop after 5 times
 
 // After 5 times. Print "Done" and let Node exit.
+
+let counter = 0;
+const interval = setInterval(
+  () => {
+    console.log('Hello World after every 5 seconds');
+    counter += 1;
+
+    if (counter === 5) {
+      console.log('Done');
+      clearInterval(interval);
+    }
+
+  },
+  5000
+);
